680b8945e1fa34871d73fe4d5d5d801f369860b5
Akash Dhruv <adhruv@gpulogin2.lcrc.anl.gov>
2024-06-08 20:06:41 -0500

    update tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,49 @@
 cd $MCFM_HOME/Bin
-make clean || true
+
+# Compile and install
+#make clean || true
 cmake -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++  ..
 make install
+
+# ===> W- processes <===
+# ===> W+ processes <===
 ./test -b u d~ ve e+
+# Directories: W
+
+# ===> W-j processes <===
+# ===> W+j processes <===
+./test -b u d~ ve e+ g
+# Directories: W1jet
+
+# ===> W-jj processes <===
+# ===> W+jj processes <===
+./test -b u d~ ve e+ g g
+# Directories: W2jet, BDK, loop
+
+# ===> Z processes <===
+./test -b u u~ e- e+
+# Directories: Z
+
+# ===> Zj processes <===
+./test -b u u~ e- e+ g
+# Directories: Z1jet, loop
+
+# ===> Zjj processes <===
+./test -b u u~ e- e+ g g
+# Directories: Z2jet, W2jet, BDK, loop
+
+# ===> h (EFT) processes <===
+./test -b -Pmodel=heft g g h
+# Directories: ggH
+
+# ===> h (SM) processes <===
+./test -b g g h
+# Directories: ggH
+
+# ===> jjj processes <===
+./test -b g g g g g
+# Directories: ThreeJets
+
+# ===> hjj (SM) processes <===
+./test -b g g h g g 
+# Directories: gghgg_dep
